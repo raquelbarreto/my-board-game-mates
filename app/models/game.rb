63da6c25f30bcb639b5179876deba9400af934dc
Marcos Mourao <marcosfrimourao@gmail.com>
2021-11-13 15:33:09 -0300

    add dependent: :destroy to models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  has_many :sessions
-  has_many :game_review
+  has_many :sessions, dependent: :destroy
+  has_many :game_review, dependent: :destroy
   has_one_attached :photo
 end
