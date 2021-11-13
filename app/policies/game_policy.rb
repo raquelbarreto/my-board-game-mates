0185a83a49e3af4b2f88ca0d1842b8ad20fe70c5
Raquel Barreto <raquelbfbarreto@gmail.com>
2021-11-13 14:45:23 +0000

    remove game policy

--- app/policies/game_policy.rb
+++ app/policies/game_policy.rb
@@ -1,27 +1,7 @@
 class GamePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(user: user)
+      scope.all
     end
-  end
-
-  def create?
-    return true
-  end
-
-  def show?
-    record.user == user
-  end
-
-  def destroy?
-    record.user == user
-  end
-
-  def edit?
-    record.user == user
-  end
-
-  def update?
-    record.user == user
   end
 end
