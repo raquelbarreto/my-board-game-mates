9221103890b845e06d3c23fdaa11f81845005b22
Raquel Barreto <raquelbfbarreto@gmail.com>
2021-11-13 14:26:54 +0000

    add adm user

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,7 +12,6 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     authorize @game
-    @game.user = current_user
     if @game.save
       redirect_to games_path
     else
--- app/policies/game_policy.rb
+++ app/policies/game_policy.rb
@@ -1,7 +1,27 @@
 class GamePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(user: user)
     end
+  end
+
+  def create?
+    return true
+  end
+
+  def show?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
   end
 end
