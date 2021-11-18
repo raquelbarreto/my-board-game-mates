5e8d047dddd8c411b7e96311abf5d3eba0a91db6
Marcos Mourao <marcosfrimourao@gmail.com>
2021-11-18 19:11:03 -0300

    add model validations

--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -1,7 +1,12 @@
 class GameSession < ApplicationRecord
+  # Associations
   belongs_to :user
   belongs_to :game
   has_one_attached :photo
   has_many :lobbies, dependent: :destroy
   has_many :users, through: :lobbies, dependent: :destroy
+
+  # Validations
+  validates :date, :time, :address, :is_smoke_free, :is_family_friendly,
+            :has_drinks, :has_food, :capacity, :is_recurrent_boolean, :session_name, presence: true
 end
