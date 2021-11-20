c2b4763c5ac03fe32653076275ad05855a509f68
Marcos Mourao <marcosfrimourao@gmail.com>
2021-11-20 12:02:18 -0300

    fix seed and validations

--- app/models/game_review.rb
+++ app/models/game_review.rb
@@ -5,5 +5,5 @@ class GameReview < ApplicationRecord
 
   # Validations
   validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
-  validates :content, presence: true, length: { in: 10..50 }
+  validates :content, presence: true, length: { minimum: 50 }
 end
