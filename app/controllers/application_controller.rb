8222c581a60176e4958e7413cf77d40760819bce
Marcos Mourao <marcosfrimourao@gmail.com>
2021-12-09 21:11:44 -0300

    add user photo to user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :about_me, :address, :age, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :about_me, :address, :age, :gender, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :about_me, :address, :age, :gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :about_me, :address, :age, :gender, :photo])
   end
 
   include Pundit
